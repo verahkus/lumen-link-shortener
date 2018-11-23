a3aace8db064b4e7af6dc322c64e4a5aa87710fe
Dmitriy Sukhov <dev@verahkus.ru>
2018-11-23 10:54:18 +0300

    mend
    install ide helper

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,7 +81,10 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
- $app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+
+if ($app->environment() !== 'production') {
+    $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+}
 
 /*
 |--------------------------------------------------------------------------
