9c96cdd223a7edfdc82d74c0111e8c46313b2055
Dmitriy Sukhov <dev@verahkus.ru>
2018-11-28 19:35:11 +0300

    change route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ $router->get('/', function () use ($router) {
     return config('app.version');
 });
 
-$router->get('short-link', [
+$router->get('short_link', [
     'as' => 'short-link', 'uses' => 'LinkController@shortLink'
 ]);
